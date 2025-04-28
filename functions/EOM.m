f8c1047c9a7bdaed43b5e2f3b47f2811686de9a2
Tejas Vinod <tejasvinod2003@gmail.com>
2025-04-28 16:22:49 -0400

    Cleaned up code

--- functions/EOM.m
+++ functions/EOM.m
@@ -1,28 +1,34 @@
 % Governing Equation of Motion
-function statedot = EOM(t, state, p_i, V_i, Cd, m, T_i, wind)
+function state_dot = EOM(t, state, p_i, V_i, C_d, m, T_i, wind)
     pos = state(1:3);
     vel = state(4:end);
     z = pos(3);
 
     Vz = V_z(z, p_i, V_i, T_i);
     Az = (Vz * 3 / (4 * pi))^(2 / 3) * pi;
-    Fasc = F_d(z, vel(3), Cd, Az);
+    Fasc = F_d(z, vel(3), C_d, Az);
     az = (L_z(z,Vz)-Fasc)/m;
 
     uvel = vel(1)-wind(1);
-    Fu = F_d(z, uvel, Cd, Az);
+    Fu = F_d(z, uvel, C_d, Az);
     au = -Fu/(m*uvel)*(vel(1)-wind(1));
 
     vvel = vel(2)-wind(2);
-    Fv = F_d(z, vvel, Cd, Az);
+    Fv = F_d(z, vvel, C_d, Az);
     av = -Fv/(m*vvel)*(vel(2)-wind(2));
-
-    statedot = [vel; au; av; az];
+    
+    if width(vel) == 3
+        state_dot = [vel'; au; av; az];
+    elseif height(vel) == 3
+        state_dot = [vel; au; av; az];
+    else
+        warning('Error')
+    end
 end
 
 % Lift at Altitude z
 function Lz = L_z(z, Vz)
-[~, rho, ~, ~] = atmosisa(z);  % get temperature and pressure at altitude z
+[~, ~, ~, rho] = atmosisa(z);  % get density at altitude z
 
 Lz = rho*Vz;
 end
@@ -31,13 +37,12 @@ end
 function Vz = V_z(z, p_i, V_i, T_i)
 [Temp, ~, P, ~] = atmosisa(z);  % get temperature and pressure at altitude z
 
-% Compute the volume at altitude z using the equation
-Vz = (p_i * V_i / T_i) * (Temp / P);  % Apply the formula
+Vz = (p_i * V_i / T_i) * (Temp / P);
 end
 
 % Drag Force
 function Fd = F_d(z, v, Cd, Az)
-[~, rho, ~, ~] = atmosisa(z);  % get temperature and pressure at altitude z
+[~, ~, ~, rho] = atmosisa(z);  % get density at altitude z
 
 Fd = 0.5*rho*v*norm(v)*Cd*Az;
 end
--- functions/runga_kutta_4.m
+++ functions/runga_kutta_4.m
@@ -1,21 +1,22 @@
-% Runge Kutta 4
-function x = runga_kutta_4(state, t, h, p_i, V_i, Cd, m, T_i, wind)
+% Rnunga Kutta 4 implementation
+function x = runga_kutta_4(state, t, h, p_i, V_i, C_d, m, T_i, wind, h_b)
     x = zeros(length(t), length(state));
-    x(1, :) = state';
+    x(1, :) = state;
     z = state(3);
 try
     for i = 1:t(end)
-        k_1 = EOM(      t(i),           x(i, :), p_i, V_i, Cd, m, T_i, wind);
-        k_2 = EOM(t(i) + h/2, x(i, :) + h*k_1/2, p_i, V_i, Cd, m, T_i, wind);
-        k_3 = EOM(t(i) + h/2, x(i, :) + h*k_2/2, p_i, V_i, Cd, m, T_i, wind);
-        k_4 = EOM(  t(i) + h,   x(i, :) + h*k_3, p_i, V_i, Cd, m, T_i, wind);
+        k_1 = EOM(      t(i),           x(i, :), p_i, V_i, C_d, m, T_i, wind);
+        k_2 = EOM(t(i) + h/2, x(i, :)' + h*k_1/2, p_i, V_i, C_d, m, T_i, wind);
+        k_3 = EOM(t(i) + h/2, x(i, :)' + h*k_2/2, p_i, V_i, C_d, m, T_i, wind);
+        k_4 = EOM(  t(i) + h,   x(i, :)' + h*k_3, p_i, V_i, C_d, m, T_i, wind);
         
         phi = (k_1 + 2*k_2 + 2*k_3 + k_4)/6;
 
         x(i+1, :) = x(i, :) + phi';
 
         
-        if (hb-x(3)) < 0
+        if (h_b-x(3)) < 0
+            warning("ballon burst")
             break
         end
 
